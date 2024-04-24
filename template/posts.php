2cc76162a6a84665dbee37ef71a1b9046fc436b4
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-24 17:21:52 +0200

    if

--- template/posts.php
+++ template/posts.php
@@ -1,5 +1,5 @@
 <ul>
-    <?php foreach($templateParams["posts"] as $post): ?>
+    <?php foreach($templateParams["posts"] as $post) {?>
     <?php
         $postOwner = $dbh->getUserByUserId($post["fkUser"]);
     ?>
@@ -9,14 +9,12 @@
                         <span><img src="<?php echo UPLOAD_DIR.$postOwner["imageUrl"]; ?>" alt="" /></span>
                         <span><a href="userProfile.php?id=<?php echo $postOwner["idUSER"]; ?>"><?php echo $postOwner["nome"]." ".$postOwner["cognome"]; ?></a></span>
                     </li>
-                    <?php if($post["mediaUrl"]!="") 
-                    {
-                        echo "<li><img src='".UPLOAD_DIR.$post["mediaUrl"]."' alt='' /></li>";
-                    }?>
-                        
+                    <?php if($post["mediaUrl"]!="") { ?>
+                         <li><img src="<?php echo UPLOAD_DIR.$post["mediaUrl"]; ?>" alt="" /></li>
+                    <php }; ?>
                     <li><a href="post.php?id=<?php echo $post["idPOST"]; ?>"><?php echo $post["text"]; ?></a></li>
                 </ul>
                 
             </li>
-    <?php endforeach; ?>
+    <?php }; ?>
 </ul>
