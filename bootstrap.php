aa87a0cb982d01e3895532e25bb176f457b051b0
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-24 17:17:18 +0200

    hf uploads

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-define("UPLOAD_DIR", "./upload/");
+define("UPLOAD_DIR", "./uploads/");
 require_once("utils/functions.php");
 require_once("db/database-helper.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "tecnologieweb2024", 3306);
