af3a02d51f5c670c1dde39ed59d0808fca2f644c
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-24 15:07:02 +0200

    hf email

--- register.php
+++ register.php
@@ -4,6 +4,7 @@ require_once 'bootstrap.php';
 if(isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["nome"]) && isset($_POST["cognome"])){
     $pepper = getPepper();
     $pwd = $_POST['password'];
+    $email = $_POST['email'];
     $nome = $_POST['nome'];
     $cognome = $_POST['cognome'];
     $pwd_peppered = hash_hmac("sha256", $pwd, $pepper);
