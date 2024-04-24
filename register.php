80f00a85f90314e34d38984daca61e1f1f61c876
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-24 15:37:14 +0200

    hf email

--- register.php
+++ register.php
@@ -6,7 +6,7 @@ if(isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["nome"]) 
         $email = $_POST['email'];
         $nome = $_POST['nome'];
         $cognome = $_POST['cognome'];
-        if(count($db->getUserByEmail())==0){
+        if(count($dbh->getUserByEmail($email))==0){
             $pepper = getPepper();
             $pwd_peppered = hash_hmac("sha256", $pwd, $pepper);
             $pwd_hashed = password_hash($pwd_peppered, PASSWORD_DEFAULT);
