0416c33bc7c2d98bf0969f8ebf101b427bae9773
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-24 17:28:04 +0200

    css

--- template/posts.php
+++ template/posts.php
@@ -1,20 +1,19 @@
 <ul>
-    <?php foreach($templateParams["posts"] as $post) {?>
-    <?php
-        $postOwner = $dbh->getUserByUserId($post["fkUser"]);
-    ?>
+    <?php foreach($templateParams["posts"] as $post) : ?>
+    <?php if($post["isComment"]==0) : ?>
+    <?php $postOwner = $dbh->getUserByUserId($post["fkUser"]);?>
             <li>
                 <ul>
                     <li>
                         <span><img src="<?php echo UPLOAD_DIR.$postOwner["imageUrl"]; ?>" alt="" /></span>
                         <span><a href="userProfile.php?id=<?php echo $postOwner["idUSER"]; ?>"><?php echo $postOwner["nome"]." ".$postOwner["cognome"]; ?></a></span>
                     </li>
-                    <?php if($post["mediaUrl"]!="") { ?>
-                         <li><img src="<?php echo UPLOAD_DIR.$post["mediaUrl"]; ?>" alt="" /></li>
-                    <php }; ?>
+                    <?php if($post["mediaUrl"]!="") : ?>
+                         <li><img class="post image" src="<?php echo UPLOAD_DIR.$post["mediaUrl"]; ?>" alt="" /></li>
+                    <?php endif; ?>
                     <li><a href="post.php?id=<?php echo $post["idPOST"]; ?>"><?php echo $post["text"]; ?></a></li>
                 </ul>
-                
             </li>
-    <?php }; ?>
+    <?php endif; ?>        
+    <?php endforeach; ?>
 </ul>
