f771cad3cc1ca5731ae54c22fd3c7e70f7efe2b6
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-26 19:05:59 +0200

    hf

--- template/layout.php
+++ template/layout.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $templateParams["titolo"]; ?></title>
     <link rel="stylesheet" type="text/css" href="./css/main.css" />
-    <script src=".javascript/functions.js"></script> 
+    <script src="javascript/functions.js"></script> 
 </head>
 <body>
     <header>
