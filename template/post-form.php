81b1a4366c1e0f4a16d747c0c8b92d64c74b2b10
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-26 18:45:24 +0200

    session user

--- template/post-form.php
+++ template/post-form.php
@@ -2,6 +2,12 @@
 <ul class="form post">
     <li class="post post-single-item">
         <ul class="collection posts detail">
+            <?php if($_SESSION["user"]["imageUrl"]!="") $userImg = UPLOAD_DIR.$_SESSION["user"]["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
+            <li class="post-element header">
+                <a href="userProfile.php?id=<?php echo $_SESSION["user"]["idUSER"]; ?>"><span><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></span>
+                <span class="user name"><?php echo $_SESSION["user"]["nome"]." ".$_SESSION["user"]["cognome"]; ?></span></a>
+            </li>
+            <li class="post-element text"><?php echo $post["text"]; ?></li>
             <li class="post-element text"><textarea name="text" cols="40" rows="5"></textarea></li>
             <li class="post-element function buttons"><button class="upload media button">Add media...</button></li>
             <li class="post-element function buttons save"><button class="save post button">Pubblica</button></li>
