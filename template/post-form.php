429d36a58f913f15a6ed4b2edabf382bff51f5e6
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-26 18:39:49 +0200

    new post form

--- template/post-form.php
+++ template/post-form.php
@@ -3,8 +3,8 @@
     <li class="post post-single-item">
         <ul class="collection posts detail">
             <li class="post-element text"><textarea name="text" cols="40" rows="5"></textarea></li>
-            <li class="post-element function buttons"><button class="upload media button">Upload...</button></li>
-            <li class="post-element timestamp">post del: <?php echo date_default_timezone_get(); ?></li>
+            <li class="post-element function buttons"><button class="upload media button">Add media...</button></li>
+            <li class="post-element function buttons save"><button class="save post button">Pubblica</button></li>
         </ul>
     </li>
 </ul>
