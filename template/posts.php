c432d81c3dc634d25c30f34c709291538e11e497
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-26 17:49:03 +0200

    hf

--- template/posts.php
+++ template/posts.php
@@ -12,7 +12,7 @@
                         <?php if($post["mediaUrl"]!="") : ?>
                             <li class="post-element media">
                                 <img class="post-element image source" src="<?php echo UPLOAD_DIR.$post["mediaUrl"]; ?>" alt="" />
-                                <span class="post-element image-background" style="background-image: <?php echo UPLOAD_DIR.$comment["mediaUrl"]; ?>" alt="" />
+                                <span class="post-element image-background" style="background-image: <?php echo UPLOAD_DIR.$post["mediaUrl"]; ?>" alt="" />
                             </li>
                         <?php endif; ?>
                         <li class="post-element function buttons"><button class="show-comment button toggle">Show comments</button><button class="add-comment button">Add comment...</button></li>
