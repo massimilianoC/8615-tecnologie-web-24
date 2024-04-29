4ef28367a959bd594e9b7a86e02e10befc70f095
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-29 11:49:52 +0200

    comments

--- template/posts.php
+++ template/posts.php
@@ -19,9 +19,27 @@
                         <li class="post-element timestamp">post del: <?php echo $post["dataInserimento"]; ?></li>
                     </ul>
                     <?php $postComments = $dbh->getCommentsByPostId($post["idPOST"]);?>
-                    <?php if(count($postComments)>0) : ?>
-                        <section class="comments">
+                        <section class="comments <?php if(count($postComments)==0){ echo " hidden";} ?>">
                             <ul class="collection posts">
+                            <li class="post comment-form hidden">
+                                <ul class="collection posts detail">
+                                    <form method="post" action="new-post.php" enctype="multipart/form-data">
+                                        <?php if($_SESSION["user"]["imageUrl"]!="") $userImg = UPLOAD_DIR.$_SESSION["user"]["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
+                                        <li class="post-element header">
+                                            <a href="userProfile.php?id=<?php echo $_SESSION["user"]["idUSER"]; ?>"><span><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></span>
+                                            <span class="user name"><?php echo $_SESSION["user"]["nome"]." ".$_SESSION["user"]["cognome"]; ?></span></a>
+                                        </li>
+                                        <li class="post-element input text"><textarea placeholder="Inserisci il tuo commento ..." name="text" cols="40" rows="5"></textarea></li>
+                                        <li class="post-element preview input media"><div class="post-element"  alt="" /></li>
+                                        <li class="hidden form input"><input type="text" id="idUSER" name="idUSER" value="<?php echo $_SESSION["user"]["idUSER"]; ?>" /></li>
+                                        <li class="hidden form input"><input type="number" id="isComment" name="isComment" value=1 /></li>
+                                        <li class="hidden form input"><input type="number" id="fkParent" name="fkParent" value=<?php echo $post["idPOST"]; ?> /></li>
+                                        <li class="post-element input function buttons"><input class="upload post media button" type="file" id="mediaUpload" name="media" accept="image/png, image/jpeg, image/jpg, video/mp4" capture/></li>
+                                        <li class="post-element save function buttons"><button class="save post button">Pubblica</button></li>
+                                    </form>
+                                </ul>
+                            </li>
+                            <?php if(count($postComments)>0) : ?>
                             <?php foreach($postComments as $comment) : ?>
                                     <?php $commentOwner = $dbh->getUserByUserId($comment["fkUser"]);?>
                                         <li class="post">
@@ -42,9 +60,9 @@
                                             </ul>
                                         </li>       
                             <?php endforeach; ?>
+                            <?php endif; ?>  
                             </ul>
-                        </section>
-                    <?php endif; ?>     
+                        </section>       
                 </li>     
     <?php endforeach; ?>
 </ul>
