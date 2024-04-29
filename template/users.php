5005900bb0b656208024b722c98cb42790285a46
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-29 17:41:50 +0200

    url

--- template/users.php
+++ template/users.php
@@ -2,7 +2,7 @@
     <h2>Utenti</h2>
     <ul>
     <?php foreach($_SESSION['template']["users"] as $utente): ?>
-        <li><a href="userProfile.php?id=<?php echo $utente["idUSER"]; ?>"><?php echo $utente["nome"]; ?> <?php echo $utente["cognome"]; ?></a></li>
+        <li><a href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>"><?php echo $utente["nome"]; ?> <?php echo $utente["cognome"]; ?></a></li>
     <?php endforeach; ?>
     </ul>
 </section>
