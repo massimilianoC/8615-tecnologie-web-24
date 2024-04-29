cfd4f22eaa5aaaaf1471f7f8508ff34abad7b9d5
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-29 15:32:17 +0200

    hf

--- template/navigation.php
+++ template/navigation.php
@@ -1,6 +1,6 @@
 <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="userProfile.php">Home</a></li>
+            <li><a href="index.php?page=userprofile&idUSER="<?php echo $_SESSION["user"]["idUSER"] ?>>My Profile</a></li>
         </ul>
 </nav>
