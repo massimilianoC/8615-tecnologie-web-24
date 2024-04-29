d8618cb6a0202dd24b907807c6d0355c1e81a9ba
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-29 15:35:48 +0200

    if user

--- template/navigation.php
+++ template/navigation.php
@@ -1,6 +1,9 @@
 <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
+            <?php if(isUserLoggedIn()) : ?>
             <li><a href="index.php?page=userprofile&idUSER="<?php echo $_SESSION["user"]["idUSER"] ?>>My Profile</a></li>
+            <li><a href="index.php?page=logout</a></li>
+            <?php endif; ?>
         </ul>
 </nav>
