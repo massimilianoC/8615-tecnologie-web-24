48574c45c8c0fce33d432817cd3f78141a1e8781
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-29 15:49:20 +0200

    fx

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 require_once("utils/constants.php");
-require_once("utils/functions.php");
 require_once("db/database-helper.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "tecnologieweb2024", 3306);
+require_once("utils/functions.php");
