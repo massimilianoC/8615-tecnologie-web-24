d486e55052dc8ecd7e14dac1ef6ec95fb0462e3b
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-29 17:56:54 +0200

    users

--- template/users.php
+++ template/users.php
@@ -1,8 +1,11 @@
 <section>
     <h2>Utenti</h2>
-    <ul>
-    <?php foreach($_SESSION['template']["users"] as $utente): ?>
-        <li><a href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>"><?php echo $utente["nome"]; ?> <?php echo $utente["cognome"]; ?></a></li>
-    <?php endforeach; ?>
+    <ul class="list-group">
+        <?php foreach($_SESSION['template']["users"] as $utente): ?>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <a href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>"><?php echo $utente["nome"]; ?> <?php echo $utente["cognome"]; ?></a>
+                <span class="badge badge-primary badge-pill">+</span>
+            </li>
+        <?php endforeach; ?>
     </ul>
 </section>
