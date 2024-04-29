b3a95394ef697fcba0def00c3a6ba0ec5a2fe51e
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-29 15:41:33 +0200

    global declaration $templateParams;

--- bootstrap.php
+++ bootstrap.php
@@ -4,3 +4,4 @@ require_once("utils/constants.php");
 require_once("utils/functions.php");
 require_once("db/database-helper.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "tecnologieweb2024", 3306);
+$templateParams;
