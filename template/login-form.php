42f8c2b6a16d1282f0c9308b247b0b0c7e9717e2
massimilianoC <massimiliano.camillucci@gmail.com>
2024-04-30 09:39:25 +0200

    form list

--- template/login-form.php
+++ template/login-form.php
@@ -2,22 +2,25 @@
     <form action="login.php" method="POST">
         <h2>Login</h2>
         <?php if(isset($_SESSION['template']["errorelogin"])): ?>
-        <p><?php echo $_SESSION['template']["errorelogin"]; ?></p>
+            <div class="alert alert-warning alert-dismissible fade show" role="alert" >
+                <?php echo $_SESSION['template']["errorelogin"]; ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
         <?php endif; ?>
-        <ul>
-            <li>
+        <ul class="list-group">
+            <li class="list-group-item" >
                 <label for="email">Email:</label><input type="text" id="email" name="email" />
             </li>
-            <li>
+            <li class="list-group-item">
                 <label for="password">Password:</label><input type="password" id="password" name="password" />
             </li>
-            <li>
-                <input type="submit" name="submit" value="Invia" />
+            <li class="list-group-item">
+                <button type="submit" class="btn btn-primary" name="submit">Invia</button>
             </li>
         </ul>
     </form>
 </section>
 <hr />
 <section>
-    <a href="index.php?page=register"><button class="btn btn-primary">Registrati</button></a>
+    <a href="index.php?page=register"><button class="btn btn-secondary">Registrati</button></a>
 </section>
