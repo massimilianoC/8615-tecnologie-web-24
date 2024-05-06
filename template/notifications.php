6a7f0aeacfca975ee0df075f77bc8d930321702b
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-06 15:05:38 +0200

    notification

--- template/notifications.php
+++ template/notifications.php
@@ -6,7 +6,17 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <!-- $_SESSION['template']["notifications"] -->
+        <?php foreach($_SESSION['template']["notifications"] as $post) : ?>
+          <ul class="list-group">
+            <li class="list-group-item">
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+              <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            </li>
+          </ul>
+        <?php endforeach; ?>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Chiudi</button>
