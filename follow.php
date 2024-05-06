9d6ecb30634cdb42df684d76cc1223b75001ad6c
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-06 16:25:49 +0200

    debug

--- follow.php
+++ follow.php
@@ -1,16 +1,16 @@
 <?php
 require_once 'bootstrap.php';
-
+var_dump($_POST);  
 if(isset($_POST["fkFollower"]) && isset($_POST["fkFollowed"])){
-
     $doUpdate = 0;
     $followed = $dbh->getFollowedByUserId($_POST['fkFollower']);
-    foreach ($followed as $user) {    
+    foreach ($followed as $user) {  
+        var_dump($user);  
         if($user['idUSER']==$_POST['fkFollowed']){
             $doUpdate = 1;
         }
     }
-    if($doUpdate== 1){
+    if($doUpdate == 1){
         //UPDATE
         $dbh->updateFollower($_POST["doFollow"],$_POST['fkFollower'],$_POST['fkFollowed']);
     }else{
@@ -22,4 +22,4 @@ if(isset($_POST["fkFollower"]) && isset($_POST["fkFollowed"])){
     }
 }
 
-header('Location: index.php?page=users');
+//header('Location: index.php?page=users');
