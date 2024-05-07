025908fa86bff9148bc10276b448a77368f80fbf
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-07 12:57:24 +0200

    btn

--- template/navigation.php
+++ template/navigation.php
@@ -2,8 +2,8 @@
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php?page=home"><i class="bi bi-chat-left-quote-fill"></i> Social Share</a>
     <?php if(isUserLoggedIn()) : ?>
-      <button class="d-none d-sm-none d-sm-block d-md-block d-lg-none btn btn-primary position-relative" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-      <i class="bi bi-bell-fill"></i><span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"><?php echo count($_SESSION['template']["notifications"]);?></span>
+      <button class="d-none d-sm-block d-md-block d-lg-none btn btn-primary position-relative" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+      <i class="bi bi-bell-fill"></i><span class="position-absolute top-50 start-100 translate-middle badge rounded-pill bg-danger"><?php echo count($_SESSION['template']["notifications"]);?></span>
       </button>
     <?php endif; ?>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,7 +12,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <?php if(isUserLoggedIn()) : ?>
-        <li><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Notifiche <span class="badge rounded-pill  text-bg-secondary"><?php echo count($_SESSION['template']["notifications"]);?></span></button></li>
+        <li class="d-sm-none d-md-none d-lg-block"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Notifiche <span class="badge rounded-pill text-bg-danger"><?php echo count($_SESSION['template']["notifications"]);?></span></button></li>
         <li class="nav-item"><a class="nav-link <?php isActive("home") ?>" href="index.php?page=home"><i class="bi bi-house"></i> <span>Home</span></a></li>
         <li class="nav-item"><a aria-current="page" class="nav-link <?php isActive("userprofile") ?>" href="index.php?page=userprofile&iduser=<?php echo $_SESSION["user"]["idUSER"] ?>"><i class="bi bi-person-circle"></i> <span>Profilo</span></a></li>
         <li class="nav-item"><a aria-current="page" class="nav-link <?php isActive("users") ?>" href="index.php?page=users"><i class="bi bi-people"></i> <span>Utenti</span></a></li>
