444fbf956fb0a30eb95b6f87644e91e944086461
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-07 11:07:31 +0200

    css fx

--- template/userProfile.php
+++ template/userProfile.php
@@ -1,7 +1,7 @@
 <section class="user-profile">
     <section class="identity">
             <?php if($_SESSION["template"]["userProfile"]["imageUrl"]!="") $userImg = UPLOAD_DIR.$_SESSION['template']["userProfile"]["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
-            <div class="user image main-profile d-flex justify-content-center"><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></div>
+            <div class="user image main-profile d-flex justify-content-center"><img class="user main-profile" src="<?php echo $userImg; ?>" alt="" /></div>
             <h1 class="user name d-flex justify-content-center"><?php echo $_SESSION['template']["userProfile"]["nome"]." ".$_SESSION['template']["userProfile"]["cognome"]; ?></h1>
     </section>
     <?php
