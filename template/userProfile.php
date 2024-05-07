7f5da61cc79cd7a9763d8259b1e90e6bf46610c4
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-07 11:06:44 +0200

    css

--- template/userProfile.php
+++ template/userProfile.php
@@ -1,13 +1,15 @@
 <section class="user-profile">
     <section class="identity">
             <?php if($_SESSION["template"]["userProfile"]["imageUrl"]!="") $userImg = UPLOAD_DIR.$_SESSION['template']["userProfile"]["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
-            <div class="user image profile"><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></div>
-            <div class="user name"><?php echo $_SESSION['template']["userProfile"]["nome"]." ".$_SESSION['template']["userProfile"]["cognome"]; ?></div>
+            <div class="user image main-profile d-flex justify-content-center"><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></div>
+            <h1 class="user name d-flex justify-content-center"><?php echo $_SESSION['template']["userProfile"]["nome"]." ".$_SESSION['template']["userProfile"]["cognome"]; ?></h1>
     </section>
     <?php
         require("template/followers.php"); 
     ?>
+    <hr/>
     <section class="posts collection">
+    <h4>Bacheca</h4>
         <?php
             if($_SESSION["user"]["idUSER"]==$_SESSION['template']["userProfile"]["idUSER"])
             {
