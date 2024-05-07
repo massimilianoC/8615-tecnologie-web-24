9c52df2a2b2654d9a26ca4403a54a40610ccb3ef
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-07 10:15:03 +0200

    hz

--- template/followers.php
+++ template/followers.php
@@ -1,8 +1,8 @@
 <section class="followers">
     <h4>Followers</h4>
-    <ul class="list-group">
+    <ul class="list-group list-group-horizontal">
         <?php foreach($_SESSION['template']["followers"] as $utente): ?>
-            <li class="list-group-item d-flex justify-content-between align-items-center user list item">
+            <li class="list-group-item">
                 <a href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>">
                 <?php if($utente["imageUrl"]!="") $userImg = UPLOAD_DIR.$utente["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
                 <span><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></span>
@@ -13,9 +13,9 @@
 </section>
 <section class="following">
     <h4>Following</h4>
-    <ul class="list-group">
+    <ul class="list-group list-group-horizontal">
         <?php foreach($_SESSION['template']["following"] as $utente): ?>
-            <li class="list-group-item d-flex justify-content-between align-items-center user list item">
+            <li class="list-group-item">
                 <a href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>">
                 <?php if($utente["imageUrl"]!="") $userImg = UPLOAD_DIR.$utente["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
                 <span><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></span>
