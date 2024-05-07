82ebbd2a16384e9703560488808140caac6ad7d7
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-07 10:44:34 +0200

    dropdown

--- template/followers.php
+++ template/followers.php
@@ -1,9 +1,19 @@
 <section class="followers">
-    <h4>Followers</h4>
-    <ul class="list-group list-group-horizontal d-flex flex-wrap">
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  Followers
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+    <h4></h4>
+    <ul class="dropdown-menu">
         <?php foreach($_SESSION['template']["followers"] as $utente): ?>
-            <li class="list-group-item user list item flex-fill">
-                <a href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>">
+            <li class="user list item ">
+                <a class="dropdown-item" href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>">
                 <?php if($utente["imageUrl"]!="") $userImg = UPLOAD_DIR.$utente["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
                 <span><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></span>
                 <?php echo $utente["nome"]; ?> <?php echo $utente["cognome"]; ?></a>
@@ -12,11 +22,13 @@
     </ul>
 </section>
 <section class="following">
-    <h4>Following</h4>
-    <ul class="list-group list-group-horizontal d-flex flex-wrap">
+<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  Following
+  </button>
+    <ul class="dropdown-menu">
         <?php foreach($_SESSION['template']["following"] as $utente): ?>
-            <li class="list-group-item user list item flex-fill">
-                <a href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>">
+            <li class="user list item">
+                <a class="dropdown-item" href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>">
                 <?php if($utente["imageUrl"]!="") $userImg = UPLOAD_DIR.$utente["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
                 <span><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></span>
                 <?php echo $utente["nome"]; ?> <?php echo $utente["cognome"]; ?></a>
