de9f787809e01bdca33b4ef14ef44fe8469b82fd
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-14 16:55:06 +0200

    hf

--- login.php
+++ login.php
@@ -3,14 +3,12 @@ require_once 'bootstrap.php';
 
 $template_data["titolo"] = "Login";
 $template_data["nome"] = "login-form.php";
-$routing = 'login';
 
 if(isset($_POST["email"]) && isset($_POST["password"])){
     $plainPassword = $_POST["password"];
     $login_result = $dbh->getUserByEmail($_POST["email"]);
 
     if(count($login_result)==0){
-        //Login fallito
         $template_data["errorelogin"] = "Errore! Controllare email o password!";
     }
     else{
@@ -21,7 +19,6 @@ if(isset($_POST["email"]) && isset($_POST["password"])){
         $pwd_hashed = $user["password"];
         if (password_verify($pwd_peppered, $pwd_hashed)) {
             registerLoggedUser($user);
-            $routing = 'home';
         }
         else {
             $template_data["errorelogin"] = "Hai dimenticato la password?";
@@ -29,6 +26,4 @@ if(isset($_POST["email"]) && isset($_POST["password"])){
     }
 }
 
-header('Location: index.php?page='.$routing);
-
-
+require 'template/base-layout.php';
