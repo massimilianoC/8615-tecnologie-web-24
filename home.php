1e9d03f4b718db0577f65ec5a82b108ff960faf4
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-14 16:24:26 +0200

    hf data

--- home.php
+++ home.php
@@ -1,7 +1,5 @@
 <?php
 require_once 'bootstrap.php';
-
-$template_data["users"] = $dbh->getUsers();
 $template_data["posts"] = $dbh->getPostsVisibleToUserId($_SESSION['user']['idUSER']);
 $template_data["titolo"] = "Home";
 $template_data["nome"] = "home-page.php";
