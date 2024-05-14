512754476808d6ba859044123eb021cdacf9ac46
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-14 16:11:04 +0200

    hf

--- template/user-profile-page.php
+++ template/user-profile-page.php
@@ -1,6 +1,6 @@
 <section class="user-profile">
     <section class="identity">
-            <?php if($_SESSION["template"]["userProfile"]["imageUrl"]!="") $userImg = UPLOAD_DIR.$template_data["userProfile"]["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
+            <?php if($template_data["userProfile"]["imageUrl"]!="") $userImg = UPLOAD_DIR.$template_data["userProfile"]["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
             <div class="user image main-profile d-flex justify-content-center"><img class="user main-profile" src="<?php echo $userImg; ?>" alt="" /></div>
             <h1 class="user name d-flex justify-content-center"><?php echo $template_data["userProfile"]["nome"]." ".$template_data["userProfile"]["cognome"]; ?></h1>
     </section>
