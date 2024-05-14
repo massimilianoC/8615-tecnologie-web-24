dd217e14ec0f1d7ade37b91c3adb790b17fabfd2
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-14 16:04:13 +0200

    home

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if(isset($_POST["email"]) && isset($_POST["password"])){
         $pwd_hashed = $user["password"];
         if (password_verify($pwd_peppered, $pwd_hashed)) {
             registerLoggedUser($user);
-            $routePage = "home";
+            header('Location: index.php?page=home');
         }
         else {
             $template_data["errorelogin"] = "Hai dimenticato la password?";
