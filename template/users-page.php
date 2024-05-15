8216538752f56b3e788f93cab07fbd963a788873
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-15 12:20:16 +0200

    type button

--- template/users-page.php
+++ template/users-page.php
@@ -17,7 +17,7 @@
                             <input id="fkFollower-<?php echo $utente["idUSER"]?>" class="hidden" type="number" name="fkFollower" value=<?php echo $_SESSION['user']['idUSER'] ?> />
                             <input id="fkFollowed-<?php echo $utente["idUSER"]?>" class="hidden" type="number" name="fkFollowed" value=<?php echo $utente["idUSER"]  ?> />
                             <input id="doAction-<?php echo $utente["idUSER"]?>" class="hidden" type="number" name="doFollow" value=<?php if($amIFollowing==1) echo 0; else echo 1 ?>  />
-                            <button id="<?php echo $utente["idUSER"]?>" class="follow-button btn-sm btn btn-outline-<?php if($amIFollowing==1) echo 'secondary'; else echo 'primary' ?>"  type="submit">
+                            <button id="<?php echo $utente["idUSER"]?>" class="follow-button btn-sm btn btn-outline-<?php if($amIFollowing==1) echo 'secondary'; else echo 'primary' ?>"  type="button">
                                 <?php if($amIFollowing==1) echo '<i class="bi bi-x-circle"></i> Unfollow'; else echo '<i class="bi bi-plus-circle"></i> Follow' ?>
                             </button>
                         </form>
