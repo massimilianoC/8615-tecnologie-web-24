24f18ed068b0afa20dd988b2061357a1f46dfcb9
massimilianoC <massimiliano.camillucci@gmail.com>
2024-05-15 11:43:11 +0200

    axios include

--- index.php
+++ index.php
@@ -2,5 +2,4 @@
 require_once 'bootstrap.php';
 require 'notifications.php';
 require("utils/routing.php");
-$template_data["js"] = array("https://unpkg.com/axios/dist/axios.min.js");
 require 'template/base-layout.php';
