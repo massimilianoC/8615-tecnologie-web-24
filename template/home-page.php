6dc7ca1a4447858779beab68a0a4a5ad7b4440b4
massimilianoC <massimiliano.camillucci@gmail.com>
2024-06-07 12:13:07 +0200

    h6

--- template/home-page.php
+++ template/home-page.php
@@ -1,7 +1,9 @@
 <section class="posts collection">
+    <h6>Nuovo Post</h6>
     <?php
         require("template/post-form.php"); 
     ?>
+    <h6>Bacheca</h6>
     <?php
         require("template/posts-archive.php"); 
     ?>
