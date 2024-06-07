05f8f1e8a82152c3ac06459467e99b3b83803e25
massimilianoC <massimiliano.camillucci@gmail.com>
2024-06-07 16:16:42 +0200

    clear errors

--- template/alerts.php
+++ template/alerts.php
@@ -1,6 +1,8 @@
 <?php if(isset($_SESSION["errorelogin"])): ?>
             <div class="alert alert-warning alert-dismissible fade show" role="alert" >
-                <?php echo $_SESSION["errorelogin"]; ?>
+                <?php echo $_SESSION["errorelogin"]; 
+                $_SESSION["errorelogin"]=""; 
+                ?>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
 <?php endif; ?>
