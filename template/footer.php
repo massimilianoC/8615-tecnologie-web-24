750f60cdef55a5b79898f3371163560b778dfac5
massimilianoC <massimiliano.camillucci@gmail.com>
2024-06-07 15:38:50 +0200

    her

--- template/footer.php
+++ template/footer.php
@@ -1,3 +1,3 @@
 <section class="footer">
-    <div class="d-flex justify-content-center"><h6>Tecnologie Web - A.A. 2023/2024</h6></div>
+    <div class="d-flex justify-content-center"><h3>Tecnologie Web - A.A. 2023/2024</h3></div>
 </section>
--- template/login-form.php
+++ template/login-form.php
@@ -19,7 +19,7 @@
 </section>
 <hr>
 <section>
-<h1>Registrazione</h1>
+<h2>Registrazione</h2>
 <ul class="list-group">
     <li class="list-group-item" >
       <span>Non sei ancora iscritto/a? </span><a class="btn btn-secondary" href="index.php?page=register">Registrati</a>
