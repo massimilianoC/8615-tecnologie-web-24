d62f05aa6719967eff8de3b3c292ff75760a2f84
massimilianoC <massimiliano.camillucci@gmail.com>
2024-06-07 17:46:37 +0200

    em

--- template/notifications.php
+++ template/notifications.php
@@ -11,15 +11,15 @@
               <?php if($notification['fkFollow']!= NULL): ?>
                 <?php $follower = $dbh->getFollowerByFollowId($notification['fkFollow']);?>
                 <li class="list-group-item alert alert-warning alert-dismissible fade show <?php if($notification['read'] > 0) echo ' read ' ; echo 'list-group-item-warning ' ?>" >
-                  <span><i class="bi bi-person-add"></i> <a href="index.php?page=userprofile&iduser=<?php echo $follower['idUSER']?>"><strong><?php echo $follower['nome'].' '.$follower['cognome']  ?></strong></a> ha iniziato a seguirti</span>
+                  <span><em class="bi bi-person-add"></em> <a href="index.php?page=userprofile&iduser=<?php echo $follower['idUSER']?>"><strong><?php echo $follower['nome'].' '.$follower['cognome']  ?></strong></a> ha iniziato a seguirti</span>
               <?php elseif($notification['fkPost']!= NULL): ?>
                 <?php $post = $dbh->getPostByPostId($notification['fkPost']);?>
                 <?php $user = $dbh->getUserByPostId($notification['fkPost']);?>
                 <li class="list-group-item alert alert-warning alert-dismissible fade show <?php if($notification['read'] > 0) echo ' read' ; echo ($post['isComment']==0)? ' list-group-item-info ' : ' list-group-item-primary ' ?>" >
                   <?php if($post['isComment']==0): ?>
-                    <span><i class="bi bi-file-post"></i><a href="index.php?page=userprofile&iduser=<?php echo $user['idUSER']?>"><strong><?php echo $user['nome'].' '.$user['cognome'] ?></strong></a> ha pubblicato un <a href="index.php#<?php echo $post['fkParent'] ?>?page=home">post</a></span>
+                    <span><em class="bi bi-file-post"></em><a href="index.php?page=userprofile&iduser=<?php echo $user['idUSER']?>"><strong><?php echo $user['nome'].' '.$user['cognome'] ?></strong></a> ha pubblicato un <a href="index.php#<?php echo $post['fkParent'] ?>?page=home">post</a></span>
                   <?php else: ?>
-                    <span><i class="bi bi-chat-dots-fill"></i> 
+                    <span><em class="bi bi-chat-dots-fill"></em> 
                     <a href="index.php?page=userprofile&iduser=<?php echo $user['idUSER']?>"><strong><?php echo $user['nome'].' '.$user['cognome']  ?></strong><?php $parent = $dbh->getPostByPostId($post['fkParent']);?></a>
                      ha commentato un <a href="index.php#post<?php echo $post['fkParent'] ?>?page=home"><?php if($parent["fkUser"]==$_SESSION["user"]['idUSER']) echo 'tuo post'; else echo 'post di '.$dbh->getUserByUserId($parent["fkUser"])['nome'] ?></a></span>
                   <?php endif; ?>
