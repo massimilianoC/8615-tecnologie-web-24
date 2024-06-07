5351c56713ed05ef35c8d0ff9c8aec5160a05487
massimilianoC <massimiliano.camillucci@gmail.com>
2024-06-07 17:43:11 +0200

    em

--- template/navigation.php
+++ template/navigation.php
@@ -13,11 +13,11 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <?php if(isUserLoggedIn()) : ?>
         <li class="d-sm-none d-md-none d-lg-block"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Notifiche <span class="notification-count badge rounded-pill text-bg-danger"><?php echo count($template_data["notifications"]);?></span></button></li>
-        <li class="nav-item"><a class="nav-link <?php isActive("home") ?>" href="index.php?page=home"><i class="bi bi-house"></i> <span>Home</span></a></li>
-        <li class="nav-item"><a aria-current="page" class="nav-link <?php isActive("userprofile") ?>" href="index.php?page=userprofile&iduser=<?php echo $_SESSION["user"]["idUSER"] ?>"><i class="bi bi-person-circle"></i> <span>Profilo</span></a></li>
-        <li class="nav-item"><a aria-current="page" class="nav-link <?php isActive("users") ?>" href="index.php?page=users"><i class="bi bi-people"></i> <span>Utenti</span></a></li>
+        <li class="nav-item"><a class="nav-link <?php isActive("home") ?>" href="index.php?page=home"><em class="bi bi-house"></em> <span>Home</span></a></li>
+        <li class="nav-item"><a aria-current="page" class="nav-link <?php isActive("userprofile") ?>" href="index.php?page=userprofile&iduser=<?php echo $_SESSION["user"]["idUSER"] ?>"><em class="bi bi-person-circle"></em> <span>Profilo</span></a></li>
+        <li class="nav-item"><a aria-current="page" class="nav-link <?php isActive("users") ?>" href="index.php?page=users"><em class="bi bi-people"></em> <span>Utenti</span></a></li>
         </ul>
-        <div class="nav-item d-flex justify-content-lg-end"><a aria-current="page" class="nav-link <?php isActive("login") ?>" href="index.php?page=logout"><div class="btn btn-danger"><i class="bi bi-power"></i> Log Out</div></a></div>
+        <div class="nav-item d-flex justify-content-lg-end"><a aria-current="page" class="nav-link <?php isActive("login") ?>" href="index.php?page=logout"><div class="btn btn-danger"><em class="bi bi-power"></em> Log Out</div></a></div>
         <?php else: ?>
         <li class="nav-item"><a aria-current="page" class="nav-link <?php isActive("login") ?>" href="index.php?page=login"><em class="bi bi-key"></em> <span>Log In</span></a></li>
         <li class="nav-item"><a aria-current="page" class="nav-link <?php isActive("register") ?>" href="index.php?page=register"><em class="bi bi-door-open"></em> <span>Register</span></a></li>
