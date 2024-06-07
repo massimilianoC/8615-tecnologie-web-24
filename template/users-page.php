74bc6d787098245817edf725fef9e6254758eaff
massimilianoC <massimiliano.camillucci@gmail.com>
2024-06-07 15:28:24 +0200

    validator utenti

--- template/users-page.php
+++ template/users-page.php
@@ -5,7 +5,7 @@
             <li class="list-group-item d-flex justify-content-between align-items-center user list item">
                 <a href="index.php?page=userprofile&iduser=<?php echo $utente["idUSER"]; ?>">
                 <?php if($utente["imageUrl"]!="") $userImg = UPLOAD_DIR.$utente["imageUrl"]; else  $userImg=DEFAULT_IMG_PROFILE ?>
-                <span><img class="user profile" src="<?php echo $userImg; ?>" alt="" /></span>
+                <span><img class="user profile" src="<?php echo $userImg; ?>" alt=""></span>
                 <?php echo $utente["nome"]; ?> <?php echo $utente["cognome"]; ?></a>
                 <?php $mySelf = $_SESSION['user']['idUSER'] ==  $utente["idUSER"] ? 1:0; ?>
                 <?php if($mySelf ==0) : ?>
@@ -14,9 +14,9 @@
                         <?php if($utente["idUSER"]==$followed["idUSER"]) $amIFollowing = 1 ?>
                     <?php endforeach; ?>
                         <form id="form-follow-<?php echo $utente["idUSER"]?>" action="#" method="POST">
-                            <input id="fkFollower-<?php echo $utente["idUSER"]?>" class="hidden" type="number" name="fkFollower" value=<?php echo $_SESSION['user']['idUSER'] ?> />
-                            <input id="fkFollowed-<?php echo $utente["idUSER"]?>" class="hidden" type="number" name="fkFollowed" value=<?php echo $utente["idUSER"]  ?> />
-                            <input id="doAction-<?php echo $utente["idUSER"]?>" class="hidden" type="number" name="doFollow" value=<?php if($amIFollowing==1) echo 0; else echo 1 ?>  />
+                            <input id="fkFollower-<?php echo $utente["idUSER"]?>" class="hidden" type="number" name="fkFollower" value=<?php echo $_SESSION['user']['idUSER'] ?>>
+                            <input id="fkFollowed-<?php echo $utente["idUSER"]?>" class="hidden" type="number" name="fkFollowed" value=<?php echo $utente["idUSER"]  ?> >
+                            <input id="doAction-<?php echo $utente["idUSER"]?>" class="hidden" type="number" name="doFollow" value=<?php if($amIFollowing==1) echo 0; else echo 1 ?>  >
                             <button id="<?php echo $utente["idUSER"]?>" class="follow-button btn-sm btn btn-<?php if($amIFollowing==1) echo 'outline-secondary'; else echo 'primary' ?>"  type="button">
                                 <?php if($amIFollowing==1) echo '<i class="bi bi-x-circle"></i> Unfollow'; else echo '<i class="bi bi-plus-circle"></i> Follow' ?>
                             </button>
